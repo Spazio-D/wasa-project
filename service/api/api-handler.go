50966d17ef74e3804cd1139f97e98e8ce9727984
Diego Spazio <spaziani.1873367@studenti.uniroma1.it>
2023-11-29 14:45:02 +0100

    Api finita (si spera)

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -6,11 +6,12 @@ import (
 
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
-	// Register routes
-	rt.router.GET("/", rt.getHelloWorld)
+	// LOGIN AND REGISTER
+	rt.router.POST("/session", rt.getHelloWorld)
 
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
 
 	return rt.router
 }
+//CIAO AMO TVB SEI  BELLISSIMO
---
+++ service/api/do-login.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (rt *_router) doLogin(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("content-type", "application/json")
+
+}
